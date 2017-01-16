37ca9651cc9d11ebe50ee915e13e2cd9cdb88a95
johnpaxton <pax@speedingplanet.com>
2017-01-16 17:24:36 -0500

    Removed code to make exercises easier

--- banking/app/payee/linked-select.directive.js
+++ banking/app/payee/linked-select.directive.js
@@ -1,27 +1,2 @@
 (function( angular ) {
-  angular.module( 'payee.directives', [ 'payee' ] )
-    .directive( 'linkedSelect', linkedSelectDirective );
-
-  function linkedSelectDirective( $log, payeeDAO ) {
-    return {
-      bindToController: true,
-      controllerAs    : '$ctrl',
-      restrict        : 'A',
-      link            : function( scope, element, attrs ) {
-        scope.$watch( attrs.linkedSelect, function( newValue ) {
-          if ( newValue ) {
-            var optionsList = '<option value="">Please select a city</option>';
-            payeeDAO.getCitiesForState( newValue )
-              .then( function( cities ) {
-                cities.forEach( function( city ) {
-                  optionsList += '<option>' + city + '</option>';
-                } );
-
-                element.html( optionsList );
-              } );
-          }
-        } )
-      }
-    }
-  }
 })( angular );
--- banking/app/payee/payee-search.component.js
+++ banking/app/payee/payee-search.component.js
@@ -1,21 +1,3 @@
 (function( angular ) {
-  angular.module( 'payee' )
-    .component( 'payeeSearch', {
-      templateUrl: 'app/payee/payee-search.component.html',
-      controller : PayeeSearchController,
-      bindings   : {
-        onSearch: '&'
-      }
-    } );
 
-  function PayeeSearchController() {
-    var ctrl = this;
-
-    ctrl.search = search;
-
-    function search( searchForm ) {
-      ctrl.onSearch( { criteria: searchForm } );
-    }
-
-  }
 })( angular );
