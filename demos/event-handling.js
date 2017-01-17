a69f412713e895e1762a9d2f8a0645f0c452d45a
johnpaxton <pax@speedingplanet.com>
2017-01-16 23:04:45 -0500

    New Demo: event handling

---
+++ demos/event-handling.js
@@ -0,0 +1,28 @@
+(function( angular ) {
+  angular.module( 'examples.events', [] )
+      .component( 'knownDirectives', {
+        templateUrl: 'event-handling.component.html',
+        controller: EventHandlingController
+      } );
+
+  function EventHandlingController( $log ) {
+    $log.log( 'Event Handling' );
+    var ctrl = this;
+
+    ctrl.clickHandler = clickHandler;
+    ctrl.enterHandler = enterHandler;
+    ctrl.leaveHandler = leaveHandler;
+
+    function clickHandler(event) {
+      $log.log( 'You clicked the button. Here is the event object: ', event );
+    }
+
+    function enterHandler() {
+      $log.log('You entered the div. Way to follow directions!')
+    }
+
+    function leaveHandler() {
+      $log.log('You left the div. We\'re sorry to see you go!')
+    }
+  }
+})( angular );
