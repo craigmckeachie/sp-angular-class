4288b53dbef5b618f1e4fa0035328fd3d17590db
johnpaxton <pax@speedingplanet.com>
2017-01-17 01:08:56 -0500

    Exercise 5 complete

--- exercises/ex-05/finish/banking/app/payee/payee-list.component.js
+++ exercises/ex-05/finish/banking/app/payee/payee-list.component.js
@@ -1,33 +1,41 @@
 (function( angular ) {
-  angular.module( 'payee.list', [] )
-      .component( 'payeeList', {
-        templateUrl: 'app/payee/payee-list-tpl.html',
-        controller: PayeeListController,
-        require: {
-          payeeComponent: '^^payeeComponent'
-        },
-        bindings: {
-          payees: '<',
-          onSelect: '&'
-        }
-      } );
+  /*
+   * Build the the Payee list component
+   * Its name is payeeList
+   * The template is found at payee-list.component.html
+   * Add a controller
+   * require the main Payee component, which should be an ancestor of this component
+   *
+   * In the controller, add an $onInit lifecycle event where this component
+   * retrieves the list of Payees from the main Payee component
+   *
+   * Also, move the event handling code from payee-main.component here
+   *
+   * When you are done, go to payee-main.component.html
+   *
+   */
+
+  angular.module( 'payee' )
+    .component( 'payeeList', {
+      templateUrl: 'app/payee/payee-list.component.html',
+      controller : PayeeListController,
+      require    : {
+        payeeMain: '^^payeeMain'
+      }
+    } );
 
   function PayeeListController( $stateParams ) {
     var ctrl = this;
 
     ctrl.$onInit = onInit;
-    ctrl.callSelect = callSelect;
+    ctrl.selectPayee = selectPayee;
 
     function onInit() {
-      if ( Object.keys( $stateParams ) > 1 ) {
-        ctrl.payeeComponent.search( $stateParams );
-      } else if ( !ctrl.payees ) {
-        ctrl.txComponent.search();
-      }
+      ctrl.payees = ctrl.payeeMain.getPayees();
     }
 
-    function callSelect( payee ) {
-      ctrl.onSelect( { payee: payee } );
+    function selectPayee( payee ) {
+      ctrl.selectedPayee = payee;
     }
 
   }
---
+++ exercises/ex-05/start/banking/app/payee/payee-list.component.js
@@ -0,0 +1,19 @@
+/*
+ * Build the the Payee list component
+ * It is part of the pre-existing payee module
+ * Its name is payeeList
+ * The template is found at payee-list.component.html
+ * Add a controller
+ * require the main Payee component, which should be an ancestor of this component
+ *
+ * In the controller, add an $onInit lifecycle event where this component
+ * retrieves the list of Payees from the main Payee component
+ *
+ * Also, move the event handling code from payee-main.component here
+ *
+ * When you are done, go to payee-main.component.html
+ *
+ */
+(function( angular ) {
+
+})( angular );
