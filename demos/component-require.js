755713f035205c1954c2290597055d238d44ed8c
johnpaxton <pax@speedingplanet.com>
2017-01-16 22:02:39 -0500

    Includes lifecycle demo of $onInit

--- demos/component-require.js
+++ demos/component-require.js
@@ -1,36 +1,49 @@
 (function( angular ) {
   angular.module( 'componentRequire', [] )
-      .component( 'levelOne', {
-        template: '<div style="padding:20px">' +
-        '<h3>Level One</h3>' +
-        '<label for="l1val">Enter a value for $ctrl.level1Value: </label>' +
-        '<input type="text" ng-model="$ctrl.level1Value" id="l1val">' +
-        '<p>$ctrl.level1Value is {{ $ctrl.level1Value }}.</p>' +
-        '<p>$ctrl.level2Value is {{ $ctrl.level2Value }}.</p>' +
-        '<level-two></level-two>' +
-        '</div>',
-        controller: function() {
-          var ctrl = this;
-          ctrl.give = function( val ) { ctrl.level2Value = val; };
-          ctrl.take = function() { return ctrl.level1Value; };
-        }
-      } )
-      .component( 'levelTwo', {
-        template: '<div style="padding:20px">' +
-        '<h3>Level Two</h3>' +
-        '<label for="l2val">Enter a value for $ctrl.level2Value: </label>' +
-        '<input type="text" ng-model="$ctrl.level2Value" id="l2val"> ' +
-        '<button ng-click="$ctrl.updateParent($ctrl.level2Value)">Update parent</button>' +
-        '<p>$ctrl.level1Value is {{ $ctrl.parent.take() }}.</p>' +
-        '<p>$ctrl.level2Value is {{ $ctrl.level2Value }}.</p>' +
-        '</div>',
-        require: {
-          parent: '^levelOne'
-        },
-        controller: function() {
-          var ctrl = this;
+    .component( 'levelOne', {
+      template  : '<div style="padding:20px">' +
+      '<h3>Level One</h3>' +
+      '<label for="l1val">Enter a value for $ctrl.level1Value: </label>' +
+      '<input type="text" ng-model="$ctrl.level1Value" id="l1val">' +
+      '<p>$ctrl.level1Value is {{ $ctrl.level1Value }}.</p>' +
+      '<p>$ctrl.level2Value is {{ $ctrl.level2Value }}.</p>' +
+      '<level-two></level-two>' +
+      '</div>',
+      controller: function() {
+        var ctrl = this;
+        ctrl.give = function( val ) {
+          ctrl.level2Value = val;
+        };
+        ctrl.take = function() {
+          return ctrl.level1Value;
+        };
+        ctrl.passedMessage = 'This is a message for level 2.';
+      }
+    } )
+    .component( 'levelTwo', {
+      template  : '<div style="padding:20px">' +
+      '<h3>Level Two</h3>' +
+      '<label for="l2val">Enter a value for $ctrl.level2Value: </label>' +
+      '<input type="text" ng-model="$ctrl.level2Value" id="l2val"> ' +
+      '<button ng-click="$ctrl.updateParent($ctrl.level2Value)">Update parent</button>' +
+      '<p>$ctrl.level1Value is {{ $ctrl.parent.take() }}.</p>' +
+      '<p>$ctrl.level2Value is {{ $ctrl.level2Value }}.</p>' +
+      '<p>$ctrl.parent.passedMessage is {{ $ctrl.parent.passedMessage }}</p>' +
+      '</div>',
+      require   : {
+        parent: '^levelOne'
+      },
+      controller: function() {
+        var ctrl = this;
+        console.log( 'At creation: levelTwo.ctrl.parent: ', ctrl.parent );
 
-          ctrl.updateParent = function(val) { ctrl.parent.give( val ); };
-        }
-      } )
+        ctrl.$onInit = function() {
+          console.log( '$onInit: levelTwo.ctrl.parent: ', ctrl.parent );
+        };
+
+        ctrl.updateParent = function( val ) {
+          ctrl.parent.give( val );
+        };
+      }
+    } )
 })( angular );
