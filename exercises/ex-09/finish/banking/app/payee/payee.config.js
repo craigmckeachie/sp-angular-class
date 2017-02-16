909b15587538126b11eb1a777b6658cb8197a74a
johnpaxton <github$U10>
2017-02-16 01:11:04 -0500

    Added extra directions

--- exercises/ex-09/finish/banking/app/payee/payee.config.js
+++ exercises/ex-09/finish/banking/app/payee/payee.config.js
@@ -12,6 +12,9 @@
    * When you are finished, load http://localhost:8000/banking/#!/payees
    * in your browser to test the results
    *
+   * Also test http://localhost:8000/banking/#!/payees/47 to make sure that
+   * the $onInit in payeeMain is working.
+   *
    */
 
   angular.module( 'payee' )
